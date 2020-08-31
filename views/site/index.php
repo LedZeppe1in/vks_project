e0944c4c2c12b257f1bfd1c439bbfa57571d57a0
Nikita Dorodnykh <tualatin32@mail.ru>
2020-08-31 13:58:47 +0800

    Initial commit

---
+++ views/site/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Система управления заявками и информирования';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Добро пожаловать!</h1>
+    </div>
+
+    <div class="body-content">
+    </div>
+</div>
